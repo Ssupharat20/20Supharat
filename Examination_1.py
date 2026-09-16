4a94f1a9e7ca9e99543fb5c0248ca6b4d7feb875
Ssupharat20 <27961@ud.ac.th>
2026-09-16 16:25:50 +0700

    Improve input prompts and fix print statement
    Updated input prompts for clarity and corrected print statement syntax.

--- Examination_1.py
+++ Examination_1.py
@@ -6,7 +6,7 @@
 
 
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
-total_minutes = int(input())
+total_minutes = int(input(จำนวนทั้งหมด))
 minutes1 = int(input(จำนวนชั่วโมง))
 minutes2 = int(input(จำนวนนาทีทีเหลือ))
 print(minutes1 /60: minutes2)
