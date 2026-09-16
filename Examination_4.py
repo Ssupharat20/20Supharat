1ce0297f86e707c7b5686c064d11db8518f26b8e
Ssupharat20 <27961@ud.ac.th>
2026-09-16 16:00:21 +0700

    Update traffic light color handling in Examination_4.py
    Fix input prompt and correct conditional statements for traffic light colors.

--- Examination_4.py
+++ Examination_4.py
@@ -6,4 +6,12 @@
 
 
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
-color = input()
+color = input("สีของไฟจราจร")
+if color ("red")
+   print("Stop")
+elif color("yellow")
+   print("Slow")
+elif color("green")
+   print("Go")
+else :
+   print("Invalid")
